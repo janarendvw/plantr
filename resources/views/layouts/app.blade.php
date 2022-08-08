906b0277e78b0a455cba71904eea1da3e5475b5b
janarendvw <janarenddvw@gmail.com>
2022-08-08 18:46:16 +0200

    added menu bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded" />
     @vite('resources/js/app.js')
     <title>Document</title>
 </head>
-<body>
+<body class="subpixel-antialiased">
+    <!-- Sidebar -->
+    <div class="sidenav w-auto p-3 flex flex-col justify-center">
+        <div class="shadow rounded-full flex flex-col p-2 jusify-center gap-3 pt-4">
+            <a>
+                <span class="material-symbols-rounded menu-item">Dashboard</span>
+            </a>
+            <a>
+                <span class="material-symbols-rounded menu-item">Person</span>
+            </a>
+            <a>
+                <span class="material-symbols-rounded menu-item">Bar_Chart</span>
+            </a>
+            <a>
+                <span class="material-symbols-rounded menu-item">Settings</span>
+            </a>
+        </div>
+    </div>
 @yield('content')
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h1>Welcome</h1>
-                    </div>
-                    <div class="card-body">
-                        <p>
-                            This is the welcome page.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+   
 @endsection
