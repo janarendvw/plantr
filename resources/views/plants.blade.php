ae3be39094fd46a177560bddbab9cce265d1addb
janarendvw <janarenddvw@gmail.com>
2022-08-09 11:53:07 +0200

    added plants page

--- resources/views/plants.blade.php
+++ resources/views/plants.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="px-10 py-5 scan-new-plant-msg m-auto flex flex-col justify-center items-center mt-36">
-        <div class="title text-center mt-4">
-{{ __('app.welcome') }}
-        </div>
-        <div class="subtitle text-black/50 text-center">
-            {{ __('app.welcome-msg-plants') }}
-        </div>
-        <div class="text text-center w-80 mt-4">
-            {{ __('app.no-plants-in-database-msg') }}
-        </div>
-        <div class="button-group flex flex-row justify-center items-center mt-4">
-            <a href="" class="button hover:button-hover text">
-               <span class="material-symbols-rounded mr-2 icon-sm">add_circle</span> {{ __('app.add-new-plant') }}
+    <div id="no-plants-in-database" class="w-96 m-auto mt-24 flex flex-col items-center justify-center p-4 text-center">
+        <div class="title">{{ __('app.welcome') }}</div>
+        <div class="subtitle">{{ __('app.welcome-msg-plants') }}</div>
+        <div class="text">{{ __('app.no-plants-in-database-msg') }}</div>
+        <div class="button-group mt-2">
+            <a class="button button-primary">
+                <span class="material-symbols-rounded icon icon-sm">
+                    add_circle
+                </span>
+                {{ __('app.add-new-plant') }}
             </a>
-            <a href="" class="button button-outline">
-                <span class="material-symbols-rounded mr-2 icon-sm">Place_item</span>   {{ __('app.import-plants') }}
+            <a class="button button-outline">
+                <span class="material-symbols-rounded icon icon-sm">
+                    place_item
+                </span>
+                {{ __('app.import-plants') }}
             </a>
-    </div>
-@endsection
+        </div>
+    @endsection
