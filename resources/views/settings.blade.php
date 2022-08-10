2e11b4e13d97f3e62eb916f5adf4ae4dd1a40de5
janarendvw <janarenddvw@gmail.com>
2022-08-10 22:30:39 +0200

    settings changes

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('page-title', __('app.pt-statistics'))
+    <div class="card dark:card-dark">
+        <div class="card-header dark:card-header-dark">{{ __('app.Settings')}}</div>
+        <div class="card-content dark: card-content-dark"></div>
+    </div>
+
+@section('content')
+
+@endsection
